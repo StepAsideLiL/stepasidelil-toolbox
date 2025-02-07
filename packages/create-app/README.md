813510482d8a421cb2527d834df9a3754e217b9b
Rifat Khan <rifat6633@gmail.com>
2025-02-07 10:23:56 +0600

    chore(create-app): initiate.

---
+++ packages/create-app/README.md
@@ -0,0 +1,3 @@
+# Create App
+
+A CLI tool to create a Nextjs app with some default configuration.
---
+++ packages/create-app/src/index.ts
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+console.log("Hello Create App from StepAsideLiL Toolbox!");
